bc76c1cb2baa4072348b717cbe80591464cc4d93
Abakour <abakour.dev@gmail.com>
2022-07-08 03:07:29 -0700

    first commit

---
+++ sendsol.js
@@ -0,0 +1,8 @@
+const web3=require("@solana/web3.js");
+(async()=>{
+	const web3=require("@solana/web3.js");
+	const connection=new web3.Connection(
+			"<YOUR_QUICKNODE_URL_HERE>",
+			'confirmed',
+		);
+})
